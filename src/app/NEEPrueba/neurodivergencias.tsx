b11f75cebf860a8cd070813c0d145b2bb78e5c44
JoseFong <jose.fong@uabc.edu.mx>
2024-06-20 01:05:45 -0700

    correcciones ligeras
    Se cambio NEE por nee al momento de hacer fetch con axios

--- src/app/NEEPrueba/neurodivergencias.tsx
+++ src/app/NEEPrueba/neurodivergencias.tsx
@@ -1,21 +1,40 @@
-"use client"
-import { Button, Input, Modal, ModalBody, ModalContent, ModalFooter, ModalHeader, Popover, PopoverContent, PopoverTrigger, useDisclosure} from '@nextui-org/react';
-import { Neurodivergencia } from '@prisma/client';
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
-import toast from 'react-hot-toast';
+"use client";
+import {
+  Button,
+  Input,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+  useDisclosure,
+} from "@nextui-org/react";
+import { Neurodivergencia } from "@prisma/client";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import toast from "react-hot-toast";
 
 function Neurodivergencias() {
-  const [neurodivergencias, setNeurodivergencias] = useState<Neurodivergencia[]>([]);
+  const [neurodivergencias, setNeurodivergencias] = useState<
+    Neurodivergencia[]
+  >([]);
   const [nombre, setNombre] = useState("");
-  const [neurodivergenciaSeleccionada, setNeurodivergenciaSeleccionada] = useState<Neurodivergencia | null>(null);
-  const { onOpen: onEditOpen, onOpenChange: onEditOpenChange, isOpen: isEditOpen } = useDisclosure();
+  const [neurodivergenciaSeleccionada, setNeurodivergenciaSeleccionada] =
+    useState<Neurodivergencia | null>(null);
+  const {
+    onOpen: onEditOpen,
+    onOpenChange: onEditOpenChange,
+    isOpen: isEditOpen,
+  } = useDisclosure();
 
   const { onOpen, onOpenChange, isOpen } = useDisclosure();
 
   const fetchNEE = async () => {
     try {
-      const response = await axios.get("/api/NEE");
+      const response = await axios.get("/api/nee");
       if (response.status === 500) return;
       setNeurodivergencias(response.data);
     } catch (e: any) {
@@ -29,11 +48,11 @@ function Neurodivergencias() {
 
   const registrar = async () => {
     const data = {
-      nombre: nombre
+      nombre: nombre,
     };
 
     try {
-      const response = await axios.post("/api/NEE", data);
+      const response = await axios.post("/api/nee", data);
       if (response.status >= 200 && response.status < 300) {
         toast.success("Se registró la neurodivergencia.");
         setNombre("");
@@ -42,7 +61,11 @@ function Neurodivergencias() {
         throw new Error(response.data.message || "Error desconocido.");
       }
     } catch (e: any) {
-      if (e.response.status === 404 || e.response.status === 500 || e.response.status === 400) {
+      if (
+        e.response.status === 404 ||
+        e.response.status === 500 ||
+        e.response.status === 400
+      ) {
         toast.error(e.response.data.message);
       } else {
         toast.error(e.message);
@@ -54,7 +77,9 @@ function Neurodivergencias() {
     if (!neurodivergenciaSeleccionada) return;
 
     try {
-      const response = await axios.delete(`/api/NEE/${neurodivergenciaSeleccionada.id}`);
+      const response = await axios.delete(
+        `/api/nee/${neurodivergenciaSeleccionada.id}`
+      );
       if (response.status >= 200 && response.status < 300) {
         toast.success("Se eliminó la neurodivergencia.");
         fetchNEE();
@@ -68,24 +93,31 @@ function Neurodivergencias() {
   };
 
   const modificar = async () => {
-    if(!neurodivergenciaSeleccionada){
-      return
-    } 
+    if (!neurodivergenciaSeleccionada) {
+      return;
+    }
 
     const data = { nombre: nombre };
     try {
-      const response = await axios.put(`/api/NEE/${neurodivergenciaSeleccionada.id}`, data);
-      if(response.status >= 200 && response.status < 300) {
-          toast.success("Se modificó la neurodivergencia.");
-          setNombre("");
-          setNeurodivergenciaSeleccionada(null);
-          fetchNEE();
-          onEditOpenChange();
+      const response = await axios.put(
+        `/api/nee/${neurodivergenciaSeleccionada.id}`,
+        data
+      );
+      if (response.status >= 200 && response.status < 300) {
+        toast.success("Se modificó la neurodivergencia.");
+        setNombre("");
+        setNeurodivergenciaSeleccionada(null);
+        fetchNEE();
+        onEditOpenChange();
       } else {
         throw new Error(response.data.message || "Error desconocido.");
       }
     } catch (e: any) {
-      if (e.response.status === 404 || e.response.status === 500 || e.response.status === 400) {
+      if (
+        e.response.status === 404 ||
+        e.response.status === 500 ||
+        e.response.status === 400
+      ) {
         toast.error(e.response.data.message);
       } else {
         toast.error(e.message);
@@ -119,39 +151,37 @@ function Neurodivergencias() {
   return (
     <div>
       <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
-            <ModalContent>
-              {(onClose) => (
-                <>
-                  <ModalHeader>
-                    ¿Seguro que desea eliminar la neurodivergencia: {neurodivergenciaSeleccionada?.nombre}?
-                  </ModalHeader>
-                  <ModalFooter>
-                    <div className="flex flex-row gap-2">
-                      <Button onPress={onClose}>Cancelar</Button>
-                      <Button color="danger" onPress={() => eliminar(onClose)}>
-                        Eliminar
-                      </Button>
-                    </div>
-                  </ModalFooter>
-                </>
-              )}
-            </ModalContent>
-      </Modal>
-
-      <Modal isOpen={isEditOpen} onOpenChange={onEditOpenChange}>
         <ModalContent>
           {(onClose) => (
             <>
               <ModalHeader>
-                Modificar Neurodivergencia
+                ¿Seguro que desea eliminar la neurodivergencia:{" "}
+                {neurodivergenciaSeleccionada?.nombre}?
               </ModalHeader>
+              <ModalFooter>
+                <div className="flex flex-row gap-2">
+                  <Button onPress={onClose}>Cancelar</Button>
+                  <Button color="danger" onPress={() => eliminar(onClose)}>
+                    Eliminar
+                  </Button>
+                </div>
+              </ModalFooter>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
+      <Modal isOpen={isEditOpen} onOpenChange={onEditOpenChange}>
+        <ModalContent>
+          {(onClose) => (
+            <>
+              <ModalHeader>Modificar Neurodivergencia</ModalHeader>
               <ModalBody>
-                <Input 
-                  type="text" 
-                  label="Nombre NEE" 
-                  placeholder="Ingresa el nombre" 
-                  value={nombre} 
-                  onChange={(e) => setNombre(e.target.value)} 
+                <Input
+                  type="text"
+                  label="Nombre NEE"
+                  placeholder="Ingresa el nombre"
+                  value={nombre}
+                  onChange={(e) => setNombre(e.target.value)}
                 />
               </ModalBody>
               <ModalFooter>
@@ -166,7 +196,6 @@ function Neurodivergencias() {
           )}
         </ModalContent>
       </Modal>
-
       Neurodivergencias
       <Popover placement="bottom" showArrow offset={10}>
         <PopoverTrigger>
@@ -178,19 +207,26 @@ function Neurodivergencias() {
               <p>No hay neurodivergencias disponibles.</p>
             ) : (
               neurodivergencias.map((neurodivergencia, index) => (
-                <li key={index}> {neurodivergencia.nombre}  
-                <button onClick={() => handleEliminar(neurodivergencia)}> eliminar </button>
-                <button onClick={() => handleModificar(neurodivergencia)}> Modificar </button>
+                <li key={index}>
+                  {" "}
+                  {neurodivergencia.nombre}
+                  <button onClick={() => handleEliminar(neurodivergencia)}>
+                    {" "}
+                    eliminar{" "}
+                  </button>
+                  <button onClick={() => handleModificar(neurodivergencia)}>
+                    {" "}
+                    Modificar{" "}
+                  </button>
                 </li>
               ))
             )}
           </ul>
-          <Input 
-            type="text" 
-            label="Nombre NEE" 
-            placeholder="Ingresa el nombre" 
-
-            onChange={(e) => setNombre(e.target.value)} 
+          <Input
+            type="text"
+            label="Nombre NEE"
+            placeholder="Ingresa el nombre"
+            onChange={(e) => setNombre(e.target.value)}
           />
           <Button onPress={handleRegistrar}>Guardar</Button>
         </PopoverContent>
@@ -200,4 +236,3 @@ function Neurodivergencias() {
 }
 
 export default Neurodivergencias;
-
