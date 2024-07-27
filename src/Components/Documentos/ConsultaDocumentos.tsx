3b314604a443f0c090e37a2ff3564e61025e2a28
JosueCV02 <josuecarreon2003@outlook.com>
2024-07-26 19:25:05 -0700

    Documentos

--- src/Components/Documentos/ConsultaDocumentos.tsx
+++ src/Components/Documentos/ConsultaDocumentos.tsx
@@ -1,12 +1,146 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Image } from "@nextui-org/react";
 import MainLayout from "../Layout/MainLayout";
+import { Card, CardHeader, CardBody } from "@nextui-org/react";
+import { useDisclosure } from "@nextui-org/react";
+import DocumentoVacioLogo from "./DocumentoVacioLogo";
+import ListaDeAsistenciaGrupo from "./ListaDeAsistenciaGrupo";
+import axios from "axios";
+import toast from "react-hot-toast";
+import ListaDeAsistenciaTodos from "./ListaDeAsistenciaTodos";
 
-function ConsultaDocumentos() {
+function ConsultaAlumnos() {
+  const [grupos, setGrupos] = useState([]);
+  const [cargando, setCargando] = useState(true);
+  const [alumnos, setAlumnos] = useState([]);
+
+  useEffect(() => {
+    fetchGrupos();
+    fetchAlumnos();
+  }, []);
+
+  // Función para obtener los grupos desde la API
+  const fetchGrupos = async () => {
+    try {
+      const response = await axios.get("/api/grupos");
+      if (response.status >= 200 && response.status < 300) {
+        setGrupos(response.data);
+        setCargando(false);
+      } else {
+        throw new Error(response.data.message || "Error desconocido.");
+      }
+    } catch (e: any) {
+      if (e.response.status === 404 || e.response.status === 500) {
+        toast.error(e.response.data.message);
+      } else {
+        toast.error(e.message);
+      }
+    }
+  };
+
+  const fetchAlumnos = async () => {
+    try {
+      const response = await axios.get("/api/alumnos");
+      if (response.status >= 200 && response.status < 300) {
+        setAlumnos(response.data);
+      } else {
+        throw new Error(response.data.message || "Error desconocido.");
+      }
+    } catch (e: any) {
+      if (e.response.status === 404 || e.response.status === 500) {
+        toast.error(e.response.data.message);
+      } else {
+        toast.error(e.message);
+      }
+    }
+  };
+
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
+  const {
+    isOpen: isOpenLista,
+    onOpen: onOpenLista,
+    onOpenChange: onOpenChangeLista,
+  } = useDisclosure();
+  const {
+    isOpen: isOpenTodos,
+    onOpen: onOpenTodos,
+    onOpenChange: onOpenChangeTodos,
+  } = useDisclosure();
+
+  const cardsData = [
+    {
+      title: "Imprimible",
+      description: "Documento vacio (Logo y firma)",
+      image: "https://i.imgur.com/22E0no7.png",
+      onOpen: onOpen,
+    },
+    {
+      title: "Imprimible",
+      description: "Lista de asistencia por grupo",
+      image: "https://i.imgur.com/GT3hn6n.png",
+      onOpen: onOpenLista,
+    },
+    {
+      title: "Imprimible",
+      description: "Lista de asistencia (Todos)",
+      image: "https://i.imgur.com/0dCI6tr.png",
+      onOpen: onOpenTodos,
+    },
+    {
+      title: "Frontend Radio",
+      description: "12 Tracks",
+      image: "https://nextui.org/images/hero-card-complete.jpeg",
+    },
+  ];
+
   return (
-    <MainLayout>
-      <div>ConsultaDocumentos</div>
-    </MainLayout>
+    <div>
+      <MainLayout>
+        <div>
+          <div className="flex flex-row m-4 md:px-10 md:pt-10 md:pb-4">
+            <div className="flex flex-col md:flex-row">
+              <h1 className="text-4xl font-bold">Documentos</h1>
+            </div>
+          </div>
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-4 m-4 ">
+            {cardsData.map((card, index) => (
+              <Card
+                key={index}
+                className="w-full"
+                isPressable
+                onPress={card.onOpen}
+              >
+                <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
+                  <p className="text-tiny uppercase font-bold">{card.title}</p>
+                  <h4 className="font-bold text-large">{card.description}</h4>
+                </CardHeader>
+                <CardBody className="overflow-visible py-2 flex items-center">
+                  <Image
+                    alt="Card background"
+                    className="object-cover rounded-xl"
+                    src={card.image}
+                    width={270}
+                  />
+                </CardBody>
+              </Card>
+            ))}
+          </div>
+        </div>
+      </MainLayout>
+      <DocumentoVacioLogo isOpen={isOpen} onOpenChange={onOpenChange} />
+      <ListaDeAsistenciaGrupo
+        alumnos={alumnos}
+        grupos={grupos}
+        isOpen={isOpenLista}
+        onOpenChange={onOpenChangeLista}
+      />
+      <ListaDeAsistenciaTodos
+        alumnos={alumnos}
+        isOpen={isOpenTodos}
+        onOpenChange={onOpenChangeTodos}
+      />
+    </div>
   );
 }
 
-export default ConsultaDocumentos;
+export default ConsultaAlumnos;
---
+++ src/Components/Documentos/ListaDeAsistenciaGrupo.tsx
@@ -0,0 +1,248 @@
+import React, { useState } from "react";
+import {
+  Modal,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+} from "@nextui-org/modal";
+import { Button } from "@nextui-org/react";
+import {
+  Document,
+  Page,
+  Text,
+  View,
+  StyleSheet,
+  PDFViewer,
+  Image,
+} from "@react-pdf/renderer";
+import { Select, SelectItem } from "@nextui-org/select";
+
+import { pdf } from "@react-pdf/renderer";
+
+const logo = "https://i.imgur.com/6hTa0ZR.png";
+
+// Estilos para el PDF
+const styles = StyleSheet.create({
+  page: {
+    flexDirection: "column",
+    backgroundColor: "#ffffff",
+    padding: 10,
+  },
+  section: {
+    margin: 10,
+    padding: 10,
+  },
+  title: {
+    fontSize: 24,
+    textAlign: "center",
+    marginBottom: 10,
+  },
+  subtitle: {
+    fontSize: 18,
+    textAlign: "center",
+    marginBottom: 10,
+  },
+  table: {
+    width: "auto",
+    borderStyle: "solid",
+    borderWidth: 1,
+    borderRightWidth: 0,
+    borderBottomWidth: 0,
+  },
+  tableRow: {
+    margin: "auto",
+    flexDirection: "row",
+  },
+  tableColName: {
+    width: "30%",
+    borderStyle: "solid",
+    borderWidth: 1,
+    borderLeftWidth: 0,
+    borderTopWidth: 0,
+  },
+  tableColDay: {
+    width: "2.258%", // (100% - 30%) / 31
+    borderStyle: "solid",
+    borderWidth: 1,
+    borderLeftWidth: 0,
+    borderTopWidth: 0,
+  },
+  tableCell: {
+    margin: "auto",
+    marginTop: 5,
+    marginBottom: 5,
+    fontSize: 9,
+  },
+  header: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    marginBottom: 10,
+    marginTop: 10,
+  },
+  headerImage: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    width: 80,
+    height: 60,
+  },
+  headerTextContainer: {
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  headerText: {
+    fontSize: 18,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  headerText2: {
+    fontSize: 8,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+});
+
+// Componente PDF
+const MyDocument = ({ grupos, grupo, alumnos }: any) => (
+  <Document>
+    <Page size="A4" style={styles.page}>
+      <View style={styles.header}>
+        <Image src={logo} style={styles.headerImage} />
+        <View style={styles.headerTextContainer}>
+          <Text style={styles.headerText}>Centros de Atención Múltiple</Text>
+          <Text style={styles.headerText2}>
+            Ana García #3899, Residencias, 21280 Imperiales, B.C.
+          </Text>
+        </View>
+      </View>
+      <View style={styles.section}>
+        <Text style={styles.title}>Lista de asistencia</Text>
+        <Text style={styles.subtitle}>
+          {grupo
+            ? `Grupo: ${grupos.find((g: any) => g.id == grupo)?.nombre}`
+            : "Todos los grupos"}
+        </Text>
+      </View>
+      <View style={styles.table}>
+        <View
+          style={[
+            styles.tableRow,
+            { backgroundColor: "#467351", color: "white" },
+          ]}
+        >
+          <View style={styles.tableColName}>
+            <Text style={styles.tableCell}>Nombre completo</Text>
+          </View>
+          {[...Array(31)].map((_, index) => (
+            <View style={styles.tableColDay} key={index}>
+              <Text style={styles.tableCell}>{index + 1}</Text>
+            </View>
+          ))}
+        </View>
+        {alumnos.map((alumno: any, index: any) => (
+          <View style={styles.tableRow} key={index}>
+            <View style={styles.tableColName}>
+              <Text style={styles.tableCell}>
+                {`${alumno.nombre.toUpperCase()} ${alumno.aPaterno.toUpperCase()} ${
+                  alumno.aMaterno ? alumno.aMaterno.toUpperCase() : ""
+                }`.trim()}
+              </Text>
+            </View>
+            {[...Array(31)].map((_, dayIndex) => (
+              <View style={styles.tableColDay} key={dayIndex}>
+                <Text style={styles.tableCell}></Text>
+              </View>
+            ))}
+          </View>
+        ))}
+      </View>
+    </Page>
+  </Document>
+);
+
+function ListaDeAsistenciaGrupo({
+  grupos,
+  alumnos,
+  isOpen,
+  onOpenChange,
+}: any) {
+  const [grupoId, setGrupoId] = useState<string>("");
+
+  const handlePrint = async () => {
+    const blob = await pdf(
+      <MyDocument grupos={grupos} grupo={grupoId} alumnos={alumnosPorGrupo()} />
+    ).toBlob();
+    const url = URL.createObjectURL(blob);
+    window.open(url, "_blank");
+  };
+
+  const handleSelectionChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setGrupoId(e.target.value);
+  };
+
+  const alumnosPorGrupo = () => {
+    if (!grupoId) return alumnos;
+    return alumnos.filter((alumno: any) => alumno.grupoId == grupoId);
+  };
+
+  return (
+    <>
+      <Modal
+        isOpen={isOpen}
+        onOpenChange={onOpenChange}
+        size="xl"
+        placement="center"
+      >
+        <ModalContent>
+          {(onClose) => (
+            <>
+              <ModalHeader className="flex flex-col gap-1">
+                Grupo a imprimir
+              </ModalHeader>
+              <ModalBody className="overflow-auto">
+                <PDFViewer width="100%" height="500px">
+                  <MyDocument
+                    grupos={grupos}
+                    grupo={grupoId}
+                    alumnos={alumnosPorGrupo()}
+                  />
+                </PDFViewer>
+                <Select
+                  label="Selecciona el grupo"
+                  className="w-full"
+                  onChange={handleSelectionChange}
+                >
+                  {grupos.map((grupo: any) => (
+                    <SelectItem key={grupo.id}>{grupo.nombre}</SelectItem>
+                  ))}
+                </Select>
+              </ModalBody>
+              <ModalFooter>
+                <Button
+                  className="bg-verde"
+                  variant="light"
+                  onPress={() => {
+                    onClose();
+                    setGrupoId("");
+                  }}
+                >
+                  Cerrar
+                </Button>
+                <Button
+                  className="bg-verdeFuerte text-[#ffffff]"
+                  onPress={handlePrint}
+                >
+                  Imprimir
+                </Button>
+              </ModalFooter>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
+    </>
+  );
+}
+
+export default ListaDeAsistenciaGrupo;
