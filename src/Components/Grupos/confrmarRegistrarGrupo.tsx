a90c5ec94ff53a8df6f88d740f26b084a09a15d0
notyisuschrist <jesus.gutierrez50@uabc.edu.mx>
2024-10-24 18:51:35 -0700

    Confirmar crear grupo (bugfix)

--- src/Components/Grupos/confrmarRegistrarGrupo.tsx
+++ src/Components/Grupos/confrmarRegistrarGrupo.tsx
@@ -1,93 +1,84 @@
-import {
-  Button,
-  Modal,
-  ModalBody,
-  ModalContent,
-  ModalFooter,
-  ModalHeader,
-} from "@nextui-org/react";
+import {Button, Modal, ModalBody, ModalContent, ModalFooter, ModalHeader,} from "@nextui-org/react";
 import axios from "axios";
 import React from "react";
 import toast from "react-hot-toast";
 
 function ConfirmarRegistrarGrupo({
-  data,
-  isOpen,
-  onOpenChange,
-  fetchGrupos,
-  fetchAlumnos,
-  reset,
-  nombreDocente,
-}: {
-  data: any;
-  isOpen: any;
-  onOpenChange: any;
-  fetchGrupos: () => void;
-  fetchAlumnos: () => void;
-  reset: () => void;
-  nombreDocente: any;
+                                     data,
+                                     isOpen,
+                                     onOpenChange,
+                                     fetchGrupos,
+                                     fetchAlumnos,
+                                     reset,
+                                     nombreDocente,
+                                 }: {
+    data: any;
+    isOpen: any;
+    onOpenChange: any;
+    fetchGrupos: () => void;
+    fetchAlumnos: () => void;
+    reset: () => void;
+    nombreDocente: any;
 }) {
-  const handleAceptar = async (onClose: any) => {
-    try {
-      data.cantidad = parseFloat(data.cantidad);
-      const response = await axios.post("/api/grupos", data);
-      if (response.status === 200) {
-        toast.success("Grupo modificado exitosamente.");
-        if (fetchGrupos && fetchAlumnos) {
-          fetchAlumnos();
-          fetchGrupos();
+    const handleAceptar = async (onClose: any) => {
+        try {
+            const response = await axios.post("/api/grupos", data);
+            if (response.status === 200) {
+                toast.success("Grupo registrado exitosamente.");
+                if (fetchGrupos && fetchAlumnos) {
+                    await fetchAlumnos();
+                    fetchGrupos();
+                }
+                // Cierra el modal
+                reset();
+                onClose();
+                onOpenChange(false);
+            } else {
+                throw new Error(response.data.message || "Error desconocido.");
+            }
+        } catch (e: any) {
+            if (e.response.status === 404 || e.response.status === 500) {
+                toast.error(e.response.data.message);
+            } else {
+                toast.error(e.message);
+            }
         }
-        // Cierra el modal
-        reset();
-        onClose();
-      } else {
-        throw new Error(response.data.message || "Error desconocido.");
-      }
-    } catch (e: any) {
-      if (e.response.status === 404 || e.response.status === 500) {
-        toast.error(e.response.data.message);
-      } else {
-        toast.error(e.message);
-      }
-    }
-  };
+    };
 
-  return (
-    <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
-      <ModalContent>
-        {(onClose) => (
-          <>
-            <ModalHeader>
-              ¿Seguro que quiere registrar el siguiente grupo?
-            </ModalHeader>
-            <ModalBody>
-              <p>
-                <span className="font-bold">Nombre: </span>
-                {data.nombre}
-              </p>
-              <p>
-                <span className="font-bold">Docente: </span>
-                {nombreDocente ?? "Sin docente asignado"}
-              </p>
-            </ModalBody>
-            <ModalFooter>
-              <div className="flex flex-row gap-2">
-                <Button onPress={onClose} className=" bg-verde ">
-                  Cancelar
-                </Button>
-                <Button
-                  className=" bg-verdeFuerte text-[#ffffff]"
-                  onPress={() => handleAceptar(onClose)}
-                >
-                  Registrar
-                </Button>
-              </div>
-            </ModalFooter>
-          </>
-        )}
-      </ModalContent>
-    </Modal>
-  );
+    return (
+        <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
+            <ModalContent>
+                {(onClose) => (
+                    <>
+                        <ModalHeader>
+                            ¿Seguro que quiere registrar el siguiente grupo?
+                        </ModalHeader>
+                        <ModalBody>
+                            <p>
+                                Nombre del grupo: {data.nombre}
+                            </p>
+                            <p>
+                                Docente: {nombreDocente}
+                            </p>
+                        </ModalBody>
+                        <ModalFooter>
+                            <div className="flex flex-row gap-2">
+                                <Button className=" bg-verde " onClick={() => onOpenChange(false)}>
+                                    Cancelar
+                                </Button>
+                                <Button
+                                    className=" bg-verdeFuerte text-[#ffffff]"
+                                    onClick={() => handleAceptar(onClose)}
+                                >
+                                    Confirmar
+                                </Button>
+                            </div>
+                        </ModalFooter>
+                    </>
+                )}
+            </ModalContent>
+        </Modal>
+    );
 }
 
 export default ConfirmarRegistrarGrupo;
--- src/Components/Grupos/registrarGrupo.tsx
+++ src/Components/Grupos/registrarGrupo.tsx
@@ -1,248 +1,260 @@
 import {
-  Button,
-  Input,
-  Modal,
-  ModalBody,
-  ModalContent,
-  ModalFooter,
-  ModalHeader,
-  Select,
-  SelectItem,
-  useDisclosure,
+    Button,
+    Checkbox,
+    Input,
+    Modal,
+    ModalBody,
+    ModalContent,
+    ModalFooter,
+    ModalHeader,
+    Select,
+    SelectItem,
+    Table,
+    TableBody,
+    TableCell,
+    TableColumn,
+    TableHeader,
+    TableRow,
+    useDisclosure,
 } from "@nextui-org/react";
-import React, { useEffect, useState } from "react";
+import React, {useEffect, useState} from "react";
 import toast from "react-hot-toast";
 import ConfirmarRegistrarGrupo from "@/Components/Grupos/confrmarRegistrarGrupo";
-import {
-  Checkbox,
-  Table,
-  TableBody,
-  TableCell,
-  TableColumn,
-  TableHeader,
-  TableRow,
-} from "@nextui-org/react";
 
 function RegistrarGrupo({
-  isOpen,
-  onOpenChange,
-  fetchGrupos,
-  fetchAlumnos,
-  docentes,
-  grupos,
-  alumnos,
-  selectedAlumnos,
-  selectedDocente,
-}: {
-  isOpen: boolean;
-  onOpenChange: (value: boolean) => void;
-  fetchGrupos: any;
-  fetchAlumnos: any;
-  docentes: any;
-  grupos: any;
-  alumnos: any;
-  selectedAlumnos: any;
-  selectedDocente: any;
+                            isOpen,
+                            onOpenChange,
+                            fetchGrupos,
+                            fetchAlumnos,
+                            docentes,
+                            grupos,
+                            alumnos,
+                            selectedAlumnos,
+                            selectedDocente,
+                        }: {
+    isOpen: boolean;
+    onOpenChange: (value: boolean) => void;
+    fetchGrupos: any;
+    fetchAlumnos: any;
+    docentes: any;
+    grupos: any;
+    alumnos: any;
+    selectedAlumnos: any;
+    selectedDocente: any;
 }) {
-  interface Alumno {
-    id: number;
-    nombre: string;
-    aPaterno: string;
-    aMaterno: string;
-    grupoId?: number | null; // Puede ser null si no está asignado a un grupo
-  }
-  interface Docente {
-    id: number;
-    nombre: string;
-  }
-  interface Grupo {
-    id: number;
-    nombre: string;
-    docenteId?: number | null; // Puede ser null si no tiene docente asignado
-    alumnosIds: number[];
-  }
-  1;
-  const [nombre, setNombre] = useState("");
-  const [docenteId, setDocenteId] = useState("");
-  const [nombreDocente, setNombreDocente] = useState("");
-  const [docentesSinGrupo, setDocentesSinGrupo] = useState([]);
-  const [alumnosDisponibles, setAlumnosDisponibles] = useState<Alumno[]>([]);
-  const [selectedAlumnosIds, setSelectedAlumnosIds] = useState<number[]>([]);
-  const [docentesDisponibles, setDocentesDisponibles] = useState<Docente[]>([]);
-
-  const {
-    onOpen: onConfirmarOpen,
-    isOpen: isConfirmarOpen,
-    onOpenChange: onConfirmarOpenChange,
-  } = useDisclosure();
-
-  useEffect(() => {
-    const docentesConGrupo = grupos
-      .filter((g: any) => g.docenteId !== null)
-      .map((g: any) => g.docenteId!); // ! para asegurar que no es null
-    const docentesDisponibles = docentes.filter(
-      (d: any) =>
-        !docentesConGrupo.includes(d.id) || d.id === selectedDocente?.id
-    );
-    const desasignarDocente = { id: -1, nombre: "Sin docente asignado" };
-    setDocentesDisponibles([desasignarDocente, ...docentesDisponibles]);
-    // Filtra a los alumnos que no tienen un grupoId o que están en el grupo seleccionado
-    const alumnosSinGrupo = alumnos.filter(
-      (a: any) =>
-        !a.grupoId ||
-        (selectedAlumnos && selectedAlumnos.some((sa: any) => sa.id === a.id))
-    );
-    setAlumnosDisponibles(alumnosSinGrupo);
-  }, [docentes, grupos, alumnos, selectedAlumnos]);
-
-  const handleRegistrar = () => {
-    if (nombre.trim() === "") {
-      toast.error("No deje campos en blanco.");
-      return;
+    interface Alumno {
+        id: number;
+        nombre: string;
+        aPaterno: string;
+        aMaterno: string;
+        grupoId?: number | null; // Puede ser null si no está asignado a un grupo
     }
 
-    if (docenteId === "") {
-      setNombreDocente("Sin docente asignado");
-      onConfirmarOpen();
-      return;
+    interface Docente {
+        id: number;
+        nombre: string;
     }
 
-    setNombreDocente(
-      docentes.find((d: any) => d.id === parseInt(docenteId)).nombre
-    );
+    interface Grupo {
+        id: number;
+        nombre: string;
+        docenteId?: number | null; // Puede ser null si no tiene docente asignado
+        alumnosIds: number[];
+    }
 
-    onConfirmarOpen();
-  };
+    const [nombre, setNombre] = useState("");
+    const [docenteId, setDocenteId] = useState("");
+    const [nombreDocente, setNombreDocente] = useState("");
+    const [docentesSinGrupo, setDocentesSinGrupo] = useState([]);
+    const [alumnosDisponibles, setAlumnosDisponibles] = useState<Alumno[]>([]);
+    const [selectedAlumnosIds, setSelectedAlumnosIds] = useState<number[]>([]);
+    const [docentesDisponibles, setDocentesDisponibles] = useState<Docente[]>([]);
 
-  const reset = () => {
-    setNombre("");
-    setDocenteId("");
-  };
+    const {
+        onOpen: onConfirmarOpen,
+        isOpen: isConfirmarOpen,
+        onOpenChange: onConfirmarOpenChange,
+    } = useDisclosure();
 
-  const handleSelectAlumno = (id: number) => {
-    setSelectedAlumnosIds((prevIds) =>
-      prevIds.includes(id)
-        ? prevIds.filter((alumnoId) => alumnoId !== id)
-        : [...prevIds, id]
-    );
-  };
+    useEffect(() => {
+        const docentesConGrupo = grupos
+            .filter((g: any) => g.docenteId !== null)
+            .map((g: any) => g.docenteId!); // ! para asegurar que no es null
+        const docentesDisponibles = docentes.filter(
+            (d: any) =>
+                !docentesConGrupo.includes(d.id) || d.id === selectedDocente?.id
+        );
+        const desasignarDocente = {id: -1, nombre: "Sin docente asignado"};
+        setDocentesDisponibles([desasignarDocente, ...docentesDisponibles]);
+        // Filtra a los alumnos que no tienen un grupoId o que están en el grupo seleccionado
+        const alumnosSinGrupo = alumnos.filter(
+            (a: any) =>
+                !a.grupoId ||
+                (selectedAlumnos && selectedAlumnos.some((sa: any) => sa.id === a.id))
+        );
+        setAlumnosDisponibles(alumnosSinGrupo);
+    }, [docentes, grupos, alumnos, selectedAlumnos]);
 
-  const handleSelectAllAlumnos = () => {
-    setSelectedAlumnosIds((prevIds) =>
-      prevIds.length === alumnosDisponibles.length
-        ? []
-        : alumnosDisponibles.map((alumno) => alumno.id)
-    );
-  };
+    const handleRegistrar = () => {
+        if (nombre.trim() === "") {
+            toast.error("No deje campos en blanco.");
+            return;
+        }
 
-  useEffect(() => {
-    sinGrupo();
-  }, [isOpen]);
+        if (docenteId === "") {
+            setNombreDocente("Sin docente asignado");
+            onConfirmarOpen();
+            return;
+        }
 
-  const sinGrupo = () => {
-    const docentesConGrupo = grupos
-      .filter((g: any) => g.docenteId != null)
-      .map((g: any) => g.docenteId);
-    setDocentesSinGrupo(
-      docentes.filter((d: any) => !docentesConGrupo.includes(d.id))
-    );
-  };
+        setNombreDocente(
+            docentes.find((d: any) => d.id === parseInt(docenteId)).nombre
+        );
 
-  return (
-    <>
-      <Modal isOpen={isOpen} onOpenChange={onOpenChange} placement="center">
-        <ModalContent>
-          <ModalHeader>Registrar Grupo</ModalHeader>
-          <ModalBody>
-            <Input
-              isRequired
-              type="text"
-              label="Nombre"
-              value={nombre}
-              onChange={(e) => setNombre(e.target.value)}
-            />
-            <Select
-              label="Docente (Opcional)"
-              labelPlacement="outside"
-              placeholder="Docente"
-              value={docenteId || ""}
-              onChange={(e) => setDocenteId(e.target.value)}
-              className="mt-8"
-              isDisabled={docentesSinGrupo.length === 0}
-              defaultSelectedKeys={[parseInt(docenteId)]}
-            >
-              {docentesSinGrupo.map((docente: any) => (
-                <SelectItem key={docente.id} value={docente.id}>
-                  {docente.nombre}
-                </SelectItem>
-              ))}
-            </Select>
-            <div className="h-80 overflow-auto p-2">
-              <Table aria-label="Lista de alumnos">
-                <TableHeader>
-                  <TableColumn className="bg-headerNav text-[#ffffff] ">
-                    Alumno
-                  </TableColumn>
-                  <TableColumn className="bg-headerNav text-[#ffffff] ">
-                    <Checkbox
-                      color="warning"
-                      isSelected={
-                        alumnosDisponibles.length === selectedAlumnosIds.length
-                      }
-                      onChange={handleSelectAllAlumnos}
-                    />
-                  </TableColumn>
-                </TableHeader>
-                <TableBody>
-                  {alumnosDisponibles.map((alumno) => (
-                    <TableRow key={alumno.id}>
-                      <TableCell>
-                        {alumno.nombre.toUpperCase()}{" "}
-                        {alumno.aPaterno.toUpperCase()}{" "}
-                        {alumno.aMaterno ? alumno.aMaterno.toUpperCase() : ""}
-                      </TableCell>
-                      <TableCell>
-                        <Checkbox
-                          color="warning"
-                          isSelected={selectedAlumnosIds.includes(alumno.id)}
-                          onChange={() => handleSelectAlumno(alumno.id)}
+        onConfirmarOpen();
+    };
+
+    const reset = () => {
+        setNombre("");
+        setDocenteId("");
+        setSelectedAlumnosIds([]);
+    };
+
+    const handleSelectAlumno = (id: number) => {
+        setSelectedAlumnosIds((prevIds) =>
+            prevIds.includes(id)
+                ? prevIds.filter((alumnoId) => alumnoId !== id)
+                : [...prevIds, id]
+        );
+    };
+
+    const handleSelectAllAlumnos = () => {
+        setSelectedAlumnosIds((prevIds) =>
+            prevIds.length === alumnosDisponibles.length
+                ? []
+                : alumnosDisponibles.map((alumno) => alumno.id)
+        );
+    };
+
+    useEffect(() => {
+        sinGrupo();
+    }, [isOpen]);
+
+    const sinGrupo = () => {
+        const docentesConGrupo = grupos
+            .filter((g: any) => g.docenteId != null)
+            .map((g: any) => g.docenteId);
+        setDocentesSinGrupo(
+            docentes.filter((d: any) => !docentesConGrupo.includes(d.id))
+        );
+    };
+
+    return (
+        <>
+            <Modal isOpen={isOpen} onOpenChange={onOpenChange} placement="center">
+                <ModalContent>
+                    <ModalHeader>Registrar Grupo</ModalHeader>
+                    <ModalBody>
+                        <Input
+                            isRequired
+                            type="text"
+                            label="Nombre"
+                            value={nombre}
+                            onChange={(e) => setNombre(e.target.value)}
                         />
-                      </TableCell>
-                    </TableRow>
-                  ))}
-                </TableBody>
-              </Table>
-            </div>
-          </ModalBody>
-          <ModalFooter>
-            <Button className=" bg-verde " onClick={() => onOpenChange(false)}>
-              Cancelar
-            </Button>
+                        <Select
+                            label="Docente (Opcional)"
+                            labelPlacement="outside"
+                            placeholder="Docente"
+                            value={docenteId || ""}
+                            onChange={(e) => setDocenteId(e.target.value)}
+                            className="mt-8"
+                            isDisabled={docentesSinGrupo.length === 0}
+                            defaultSelectedKeys={[parseInt(docenteId)]}
+                        >
+                            {docentesSinGrupo.map((docente: any) => (
+                                <SelectItem key={docente.id} value={docente.id}>
+                                    {docente.nombre}
+                                </SelectItem>
+                            ))}
+                        </Select>
+                        <div className="h-80 overflow-auto p-2">
+                            <Table aria-label="Lista de alumnos">
+                                <TableHeader>
+                                    <TableColumn className="bg-headerNav text-[#ffffff] ">
+                                        Alumno
+                                    </TableColumn>
+                                    <TableColumn className="bg-headerNav text-[#ffffff] ">
+                                        <Checkbox
+                                            color="warning"
+                                            isSelected={
+                                                alumnosDisponibles.length === selectedAlumnosIds.length
+                                            }
+                                            onChange={handleSelectAllAlumnos}
+                                        />
+                                    </TableColumn>
+                                </TableHeader>
+                                <TableBody>
+                                    {alumnosDisponibles.map((alumno) => (
+                                        <TableRow key={alumno.id}>
+                                            <TableCell>
+                                                {alumno.nombre.toUpperCase()}{" "}
+                                                {alumno.aPaterno.toUpperCase()}{" "}
+                                                {alumno.aMaterno ? alumno.aMaterno.toUpperCase() : ""}
+                                            </TableCell>
+                                            <TableCell>
+                                                <Checkbox
+                                                    color="warning"
+                                                    isSelected={selectedAlumnosIds.includes(alumno.id)}
+                                                    onChange={() => handleSelectAlumno(alumno.id)}
+                                                />
+                                            </TableCell>
+                                        </TableRow>
+                                    ))}
+                                </TableBody>
+                            </Table>
+                        </div>
+                    </ModalBody>
+                    <ModalFooter>
+                        <Button className=" bg-verde " onClick={() => onOpenChange(false)}>
+                            Cancelar
+                        </Button>
 
-            <Button
-              onClick={handleRegistrar}
-              className=" bg-verdeFuerte text-[#ffffff]"
-            >
-              Registrar
-            </Button>
-          </ModalFooter>
-        </ModalContent>
-      </Modal>
-      <ConfirmarRegistrarGrupo
-        data={{
-          nombre,
-          docenteId: docenteId || "",
-          selectedAlumnosIds: selectedAlumnosIds,
-        }}
-        isOpen={isConfirmarOpen}
-        nombreDocente={nombreDocente}
-        onOpenChange={onConfirmarOpenChange}
-        fetchGrupos={fetchGrupos}
-        fetchAlumnos={fetchAlumnos}
-        reset={reset}
-      />
-    </>
-  );
+                        <Button
+                            onClick={handleRegistrar}
+                            className=" bg-verdeFuerte text-[#ffffff]"
+                        >
+                            Registrar
+                        </Button>
+                    </ModalFooter>
+                </ModalContent>
+            </Modal>
+            <ConfirmarRegistrarGrupo
+                data={{
+                    nombre,
+                    docenteId: docenteId || null, // Asegúrate de que sea null si no hay docente
+                    selectedAlumnosIds: selectedAlumnosIds,
+                }}
+                isOpen={isConfirmarOpen}
+                nombreDocente={nombreDocente}
+                onOpenChange={(isOpen: any) => {
+                    onConfirmarOpenChange();
+                    if (!isOpen) onOpenChange(false); // Cierra el modal de registro
+                }}
+                fetchGrupos={fetchGrupos}
+                fetchAlumnos={async () => {
+                    await fetchAlumnos();
+                    const alumnosSinGrupo = alumnos.filter(
+                        (a: any) =>
+                            !a.grupoId ||
+                            (selectedAlumnos && selectedAlumnos.some((sa: any) => sa.id === a.id))
+                    );
+                    setAlumnosDisponibles(alumnosSinGrupo);
+                }}
+                reset={reset}
+            />
+        </>
+    );
 }
 
 export default RegistrarGrupo;
