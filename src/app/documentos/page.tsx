9bf2fd294958892296911ba62f6075a925e997bb
JoseFong <jose.fong@uabc.edu.mx>
2025-06-18 11:22:50 -0700

    ELIMINACIÓN DE DOCUMENTOS TEMPORAL

--- src/app/documentos/page.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-import React from "react";
-import ConsultaDocumentos from "@/Components/Documentos/ConsultaDocumentos";
-
-function page() {
-  return <ConsultaDocumentos />;
-}
-
-export default page;
